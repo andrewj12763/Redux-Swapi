0fec894ed63626702bf13e26157c58e10c216f9e
slaforsjov <andrewj12763@gmail.com>
2019-07-15 18:53:48 -0600

    addid most functions. Still have an error preventing display

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,27 @@
-// we'll need axios
+import axios from 'axios';
 
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
+export const FETCHING = "FETCHING";
+export const SUCCESS = "SUCCESS";
+export const ERROR = "ERROR";
+
 
 // our action creator will be a function that returns a function
-// the url to fetch characters from is `https://swapi.co/api/people/`
+export const getChar = () => dispatch => {
+    dispatch({type: FETCHING});
+
+    // the url to fetch characters from is `https://swapi.co/api/people/`
+
+    axios
+    .get("https://swapi.co/api/people/")
+    .then(res => {
+      console.log(res.data.results);
+      dispatch({ type: SUCCESS, payload: res.data.results });
+    })
+    .catch(err => {
+      dispatch({type: ERROR, payload: err.response.data.error.message});
+    });
+};
 // remember that now we have controll over our thunk-based action creator
+
